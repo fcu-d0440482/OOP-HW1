408ae374faaf064249f7a96a2affa77b4cc81937
user <user@123.110.12.218>
2016-10-03 21:21:46 +0800

    Hw1

---
+++ src/fcu/iecs/oop/Hw1.java
@@ -0,0 +1,12 @@
+package fcu.iecs.oop;
+
+public class Hw1 {
+
+	public static void main(String[] args) {
+		String lyics = "Let it go! Let it go! Cannot hold it back anymore";
+		lyics = lyics.replace("it","\"her\"");
+		lyics = lyics.replace("Cannot","can't");
+		System.out.println(lyics);
+	}
+
+}
